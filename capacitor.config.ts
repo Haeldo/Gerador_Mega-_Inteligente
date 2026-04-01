85960e8c1cadd79fa109d4672d0bc73c5c4a087b
Haeldo <102492938+Haeldo@users.noreply.github.com>
2026-04-01 12:03:32 +0000

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.haeldo.geradormega',
+  appName: 'Gerador Mega Inteligente',
+  webDir: 'dist'
+};
+
+export default config;
