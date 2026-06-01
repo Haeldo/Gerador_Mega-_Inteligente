2773cb9e8759cbfe8b7be4948b42f89133120f00
Haeldo <102492938+Haeldo@users.noreply.github.com>
2026-06-01 17:32:07 +0000

    Criar APK

--- android/app/src/main/java/com/haeldo/geradormega/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.haeldo.geradormega;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.haeldo.geradormega',
-  appName: 'Gerador Mega Inteligente',
-  webDir: 'dist'
-};
-
-export default config;
